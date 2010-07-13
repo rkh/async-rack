726bdde13028b8a83b0b0b47e0aa67b309fcce0b
Rafi Jacoby <rjacoby@astrokid.com>
2010-07-13 10:40:47 +0800

    fixed start_time typo

--- lib/async_rack/runtime.rb
+++ lib/async_rack/runtime.rb
@@ -4,7 +4,7 @@ module AsyncRack
   class Runtime < AsyncCallback(:Runtime)
     def async_callback(result)
       status, headers, body =result
-      request_time = Time.now - start_time
+      request_time = Time.now - @start_time
       headers[@header_name] = "%0.6f" % request_time if !headers.has_key?(@header_name)
       [status, headers, body]
     end
